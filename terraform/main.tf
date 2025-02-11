216178d9ab65c4d246c16744a52b47ae265ae3b6
Mikhail Kurenkov <kool.thing@yandex.com>
2025-02-11 08:38:52 +0600

    add terraform balancer target group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,33 @@
-module "yc-vpc" {
-  source              = "github.com/terraform-yc-modules/terraform-yc-vpc.git"
-  network_name        = "test-module-network"
-  network_description = "Test network created with module"
-  private_subnets = [{
-    name           = "subnet-1"
-    zone           = "ru-central1-a"
-    v4_cidr_blocks = ["10.10.0.0/24"]
-    },
-    {
-      name           = "subnet-2"
-      zone           = "ru-central1-b"
-      v4_cidr_blocks = ["10.11.0.0/24"]
-    },
-    {
-      name           = "subnet-3"
-      zone           = "ru-central1-d"
-      v4_cidr_blocks = ["10.12.0.0/24"]
-    }
-  ]
-}
+# module "yc-vpc" {
+#   source              = "github.com/terraform-yc-modules/terraform-yc-vpc.git"
+#   network_name        = "default"
+#   network_description = "Default network"
+#   private_subnets = [{
+#     name           = "subnet-1"
+#     zone           = "ru-central1-a"
+#     v4_cidr_blocks = ["10.10.0.0/24"]
+#     },
+#     {
+#       name           = "subnet-2"
+#       zone           = "ru-central1-b"
+#       v4_cidr_blocks = ["10.11.0.0/24"]
+#     },
+#     {
+#       name           = "subnet-3"
+#       zone           = "ru-central1-d"
+#       v4_cidr_blocks = ["10.12.0.0/24"]
+#     }
+#   ]
+# }
 
 module "kube" {
   source     = "github.com/terraform-yc-modules/terraform-yc-kubernetes.git"
-  network_id = module.yc-vpc.vpc_id
+  network_id = var.network_id
 
   master_locations = [
-    for s in module.yc-vpc.private_subnets :
     {
-      zone      = s.zone,
-      subnet_id = s.subnet_id
+      zone      = "ru-central1-d"
+      subnet_id = var.subnet_id
     }
   ]
 
@@ -50,6 +49,21 @@ module "kube" {
         role        = "worker-01"
         environment = "prod"
       }
+      nat = true
     }
   }
+<<<<<<< HEAD
+=======
+  cluster_name = var.cluster_name
+  public_access = true
+  
+  # custom_ingress_rules = {
+  #   "rule1" = {
+  #     protocol = "TCP"
+  #     description = "rule-1"
+  #     v4_cidr_blocks = ["0.0.0.0/0"]
+  #     port = 80
+  #   }
+  # }
+>>>>>>> 7177436 (add terraform balancer target group)
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,18 @@ variable "size" {
   default     = 2
   description = "Size of the instance"
 }
+variable "cluster_name" {
+  type        = string
+  default     = "momo-store"
+  description = "Name of the Kubernetes cluster"  
+}
+variable "subnet_id" {
+  type        = string
+  default     = "fl86j1k5qddvoef4e9hq"
+  description = "ID of the subnet"
+}
+variable "network_id" {
+  type        = string
+  default     = "enpuhc98a2jpm6jjrsfh"
+  description = "ID of the network"
+}
